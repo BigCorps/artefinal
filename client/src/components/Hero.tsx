0b2a534dc07e9308bde3230f782543b496e2d2de
BigCorps <interpag.bigcorps@gmail.com>
2026-06-19 20:05:23 -0300

    Atualizar o Hero.tsx

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -28,7 +28,7 @@ export default function Hero() {
               </h1>
 
               <p className="text-lg text-gray-600 leading-relaxed">
-                Converta qualquer arquivo, transforme logos em SVG, crie modelos 3D, redimensione imagens e muito mais. Tudo que uma empresa gráfica, papelaria e estúdio de design precisa para editar arquivos rapidamente e enviar para produção.
+                Converta arquivos, crie sangrias, marcas de corte, transforme logos em SVG, crie modelos 3D, redimensione imagens, QR codes, código de barras, orçamentos e muito mais. Tudo que uma empresa gráfica, papelaria e estúdio de design precisa para editar arquivos rapidamente e enviar para produção. Interagindo com o assistente, você pode descobrir como ele pode ajudar a sua produção gráfica a ser mais ágil e prática.
               </p>
             </div>
 
